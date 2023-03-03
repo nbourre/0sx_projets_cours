d7b2799c78cf23879420b41948b0cd0033bc2c9c
Nick B <nbourrereg@hotmail.com>
2023-03-03 10:57:45 -0500

    Ajout du rebond

---
+++ c03_bouton_rebond/c03_bouton_rebond.ino
@@ -0,0 +1,57 @@
+// Exemple montrant comment détecter 
+// le front montant et descendant 
+// du signal du bouton
+//
+
+
+#define BTN_PIN 4
+
+unsigned long currentTime = 0;
+unsigned long lastTime = 0;
+
+// Lorsque la lecture = 1, le bouton
+// n'est pas appuyé
+int btnPrecedent = 1;
+
+// L'état utilisé pour la logique de l'application
+int btnEtat = 1;
+int btnEtatPrec = 1;
+
+// Delai pour eviter les rebonds
+int btnDelai = 25;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(BTN_PIN, INPUT_PULLUP);
+
+}
+
+void loop() {
+  currentTime = millis();
+  
+  int btnActuel = digitalRead(BTN_PIN);
+  
+  if (btnActuel != btnPrecedent) {
+    lastTime = currentTime;        
+  }
+  
+  // Ce petit delai permet d'éviter de lire un rebond
+  if (currentTime - lastTime > btnDelai) {
+    if (btnActuel != btnEtat) {
+      btnEtat = btnActuel; // On enregistre l'état actuel
+      Serial.print("Etat actuel:");
+      Serial.println(btnEtat);
+    }
+  }
+  
+  btnPrecedent = btnActuel;
+  
+  if (btnEtat == 1 && btnEtatPrec == 0) {
+    Serial.println("Bouton relache");
+  } else if (btnEtat == 0 && btnEtatPrec == 1) {
+    Serial.println("Bouton appuye");
+  }
+  
+  btnEtatPrec = btnEtat;
+}

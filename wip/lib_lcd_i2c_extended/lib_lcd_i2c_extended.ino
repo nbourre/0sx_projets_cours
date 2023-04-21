fe7e73b302dbc01f623938990675de7b56dad4da
Nick B <nbourrereg@hotmail.com>
2023-04-20 21:36:54 -0400

    Ajout d'une fonction de string

--- wip/lib_lcd_i2c_extended/lib_lcd_i2c_extended.ino
+++ wip/lib_lcd_i2c_extended/lib_lcd_i2c_extended.ino
@@ -10,11 +10,20 @@ char * testCallbacks2() {
   return "Apple!";
 }
 
+char * testStringCb() {
+  static String result = "Mucho potatoes!";
+  static char _buff[20] = "";
+  result.toCharArray(_buff, result.length());
+  
+  return _buff;
+}
+
 void setup() {
   Serial.begin(9600);
   //lcd.init();
   lcd.addCallback(testCallbacks);
   lcd.addCallback(testCallbacks2);  
+  lcd.addCallback(testStringCb);
 }
 
 void loop() {
